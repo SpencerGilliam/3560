c3c0e15c7045b74da67fa41c4153c08f7cdbfca6
Michael Cooper <michaelcooper99@hotmail.com>
2019-04-12 00:28:35 -0400

    Added many settings with ways to access them. Now opening the JSON file and creating a dict, then sending every keyword to the findFuncDec function.
    Signed-off-by: Michael Cooper <michaelcooper99@hotmail.com>

--- backendInteract.py
+++ backendInteract.py
@@ -9,12 +9,40 @@ import json
 
 #store file content in line array and insert comments using list.insert(index, obj)
 
+#LANGUAGE will be set by the frontend
+LANGUAGE = "Python"		#default Python
+
+FILES = []
+
+def setLang(lang):
+	LANGUAGE = lang
+
+def setFiles(files):
+	for f in files:
+		FILES.append(f)
+
 fileContent = \
 	["def Lorem ipsum dolor sit amet, consecteturdef adipiscing elit.",
 	 "Integer luctus dictum leo defvitae gravida.",
+	 "int main",
+	 "main int",
 	 "def Sed placerat turpis def eu odio fermentum maximus.",
 	 "Sed eu facilisis def purus."]
 
 
-dc.findFuncDec(fileContent, "def", 0)	#will return lines OF declaration, work BACKWARDS with these to avoid doing
-										#extra unneeded math to get new line of each function after adding lines
+
+with open('languageKeywords.json', encoding='utf-8') as langInfo:
+	data = json.loads(langInfo.read())
+
+
+
+definers = []
+
+for i in data[LANGUAGE]:
+	definers.append(("^" + i))
+	# to find all function declarations just look for function keywords at beginning of line following names and then parenthesis MAYBE
+
+lines = dc.findFuncDec(fileContent, definers)	#will return lines OF declaration, work BACKWARDS with these to avoid doing
+											#extra unneeded math to get new line of each function after adding lines
+for line in lines:
+	print("Function Declared at line:", line)
