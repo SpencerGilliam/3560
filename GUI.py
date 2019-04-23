7f4510febee6906b93d620ab5dac6dcc6eecae32
SpencerGilliam <38114786+SpencerGilliam@users.noreply.github.com>
2019-04-23 10:09:17 -0400

    Language Drop Down Menu
    Next:
    Creating a text box that take file name input
    
    Signed-off-by: SpencerGilliam <38114786+SpencerGilliam@users.noreply.github.com>

--- GUI.py
+++ GUI.py
@@ -1,8 +1,9 @@
-<<<<<<< HEAD
+import tkinter
 from tkinter import *
 from tkinter import filedialog
 import pprint  # added import
 import tkinter as tk
+from tkinter.filedialog import askopenfilename
 from tkinter.ttk import Combobox
 
 filelist = []  # added line
@@ -11,30 +12,29 @@ root.geometry("500x500")
 
 mb = Menubutton(root, text="PyAutoDoc Menu")
 
-button = Button(text="Languages", width=30, command=lambda: Language)
-button.place(relx=0.5, rely=0.5, anchor=CENTER)
 mb.pack()
 
 
 #################################################################################################################
-class Language(tk.Frame):
+class Language(tk.Frame):  # Allow you to choose your language
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
         choiceVar = tk.StringVar()
-        choices = ("Python", "C++", "C", "Ruby")
+        choices = ("Python", "C++", "C", "Ruby") #Language Choices
         choiceVar.set(choices[0])
 
-        cb = Combobox(self, textvariable=choiceVar, values=choices)
+        cb = Combobox(self, textvariable=choiceVar, values=choices, state="readonly") # Drop down menu
 
         cb.pack()
 
-
+button = Button(text="Languages", width=30, command=lambda: Language)
+button.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 if __name__ == "__main__":
     Language(root).pack(fill="both", expand=True)
 
 ###############################################################################################################
-def OpenFile(root, filelist):
+def OpenFile(root, filelist): # Select Files function, produces a button, and allows you to choose multiple files
     filez = filedialog.askopenfilenames(parent=root, title='Select files')
     filez = root.tk.splitlist(filez)
     filelist += filez  # added line
@@ -44,6 +44,9 @@ def OpenFile(root, filelist):
 
 button = Button(text="Select Files", width=30, command=lambda: OpenFile(root, filelist))
 button.pack(padx=25, pady=20, side=tk.TOP)
+##############################################################################################################
+def FileBox ():
+
 
 root.mainloop()
 
@@ -64,36 +67,3 @@ root.mainloop()
 # menu.add_cascade(label="Edit", menu=editMenu)
 # editMenu.add_command(label="redo", command=doNothing)
 
-root.mainloop()
-=======
-from tkinter import*
-from tkinter import Menu
-from tkinter import filedialog
-import pprint #added import
-
-filelist = [] #added line
-root = Tk()
-root.geometry("800x500")
-
-mb = Menubutton(root, text="PyAutoDoc Menu")
-mb.menu = Menu(mb)
-mb["menu"] = mb.menu
-
-mb.menu.add_command(label="Python", command=lambda: print("Python"))
-mb.menu.add_command(label="C++", command=lambda: print("C++"))
-
-mb.pack()
-
-
-def OpenFile(root, filelist):
-    filez = filedialog.askopenfilenames(parent=root,title='Select files')
-    filez = root.tk.splitlist(filez)
-    filelist += filez #added line
-    pp = pprint.PrettyPrinter(indent=0) #added line
-    pp.pprint (filelist) #added line
-
-button = Button(text="Select Files",width = 30,command=lambda: OpenFile(root, filelist))
-button.pack()
-
-root.mainloop()
->>>>>>> origin/GUI
