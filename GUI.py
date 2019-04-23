038224b13097397979da622941b64ec712e2031e
SpencerGilliam <38114786+SpencerGilliam@users.noreply.github.com>
2019-04-23 11:03:08 -0400

    Creating a text box that take file name input
    Learning how to use a text widget have tried multiple examples but it will not show on the menu without the widget I assume.
    
    Signed-off-by: SpencerGilliam <38114786+SpencerGilliam@users.noreply.github.com>

--- GUI.py
+++ GUI.py
@@ -15,12 +15,13 @@ mb = Menubutton(root, text="PyAutoDoc Menu")
 mb.pack()
 
 
-#################################################################################################################
+########################################################################################################################
 class Language(tk.Frame):  # Allow you to choose your language
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
         choiceVar = tk.StringVar()
-        choices = ("Python", "C++", "C", "Ruby") #Language Choices
+        choices = ("Python", "C++", "C", "Ruby","C#","Go","Java","Javascript" #Maybe Set up dynamically later
+                   ,"PHP","Kotlin","Scala","Haskell","Lua","Rust","Perl") #Language Choices
         choiceVar.set(choices[0])
 
         cb = Combobox(self, textvariable=choiceVar, values=choices, state="readonly") # Drop down menu
@@ -32,8 +33,12 @@ button.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 if __name__ == "__main__":
     Language(root).pack(fill="both", expand=True)
+########################################################################################################################
+def FileBox ():
+    #Text box is not showing up
 
-###############################################################################################################
+
+########################################################################################################################
 def OpenFile(root, filelist): # Select Files function, produces a button, and allows you to choose multiple files
     filez = filedialog.askopenfilenames(parent=root, title='Select files')
     filez = root.tk.splitlist(filez)
@@ -44,11 +49,9 @@ def OpenFile(root, filelist): # Select Files function, produces a button, and al
 
 button = Button(text="Select Files", width=30, command=lambda: OpenFile(root, filelist))
 button.pack(padx=25, pady=20, side=tk.TOP)
-##############################################################################################################
-def FileBox ():
-
-
 root.mainloop()
+########################################################################################################################
+
 
 # def doNothing():
 #         print("I won`t")
