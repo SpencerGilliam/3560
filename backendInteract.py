6e2029922c3dc8c23d96552b18bb9057ba0bbb52
Michael Cooper <mc454916@ohio.edu>
2019-04-23 21:37:48 -0400

    nearly final version of backend
    Signed-off-by: Michael Cooper <mc454916@ohio.edu>

--- backendInteract.py
+++ backendInteract.py
@@ -14,23 +14,28 @@ LANGUAGE = "Python"		#default Python
 
 FILES = []
 
+#call like bi.setLang
 def setLang(lang):
     global LANGUAGE
     LANGUAGE = lang
 
+#call like bi.setFiles(fileList)
 def setFiles(files):
     for f in files:
         FILES.append(f)
 
+#do NOT call this one
 def getFileContent(file):
 	content = ""
 	with open(file) as f:
 		content = f.readlines()
-	content = [tmp.strip('\n\t') for tmp in content]
+	content = [tmp.strip('\n') for tmp in content]
 	return content
 
 #print(getFileContent("languageKeywords.json"))
 
+#call this in child window when user accepts their comment input like addComment(curFile, commentList, index)
+						#get index arg from dict that is returned by getLines
 def addComment(file, comment, index):
 	newContent = dc.comment(getFileContent(file), comment, index)
 	f = open(file, 'w')
@@ -38,6 +43,7 @@ def addComment(file, comment, index):
 	if newContent != "machine broke error":
 		f.write(newContent)
 
+#do NOT call this one
 def getDefiners(lang):
 	with open('languageKeywords.json', encoding='utf-8') as langInfo:
 		data = json.loads(langInfo.read())
@@ -45,17 +51,17 @@ def getDefiners(lang):
 	definers = []
 
 	for i in data[lang]:
-		definers.append(("^" + i))
+		definers.append(("(^|\t| )" + i))
 		print(i)
 	return definers
 
+#do NOT call this one
 def getClasses(definers):
 	content = []
 	for file in FILES:
 		content = getFileContent(file)
 		lines = []
-		tmpDef = ["^class"]
-		lines = dc.findFuncDec(content, tmpDef)
+		lines = dc.findFuncDec(content, definers)
 
 		linesDict = dict()
 		for line in lines:
@@ -66,7 +72,8 @@ def getClasses(definers):
 			print(key, linesDict[key])
 
 	return definers
-# to find all function declarations just look for function keywords at beginning of line following names and then parenthesis MAYBE
+
+#call from GUI in child window process while asking to comment
 def getLines(file, definers):
 	fileContent = getFileContent(file)
 	lines = dc.findFuncDec(fileContent, definers)	#will return lines OF declaration, work BACKWARDS with these to avoid doing
@@ -82,29 +89,3 @@ def getLines(file, definers):
 
 def getCustomDataTypes(files, definers):
 	pass	#for each file in files, open, read, look for class/struct declarations. return new definers list
-
-FILES.append("Test Code\doctumentorTest.py")
-FILES.append("Test Code\doumentorTest2.py")
-definers = getDefiners("Python")
-if definers[0] != "def":
-	definers = getClasses(definers)
-
-content = []
-for i in range(FILES.__len__()):
-	content.append(getLines(FILES[i], definers))
-
-comment = ["#test line 1 appear first", "#test line 2 appear second"]
-#soon replace # in string with commentDef[LANG]
-cont = []
-for i in range(0, FILES.__len__()):
-	cont.append(list(reversed(sorted(content[i].keys()))))
-coms = []
-i = 0
-for f in FILES:
-	for key in cont[i]:
-		comment = input("comment for line " + str(content[i][key]) + " -> ")
-		comment = "#" + comment
-		coms.append(comment)
-		addComment(f, coms, key)
-		coms.clear()
-	i += 1
