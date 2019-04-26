d2dd6d5deaba025920e169cf96e7622d2d746528
SpencerGilliam <38114786+SpencerGilliam@users.noreply.github.com>
2019-04-26 15:54:53 -0400

    Formatted win2 text box, Set the comment block format up, and added 'No Comment' and '>>' buttons to win2.
    Next Steps:
    
    Fixing where if you press a button (in the root menu) more than once it
    will only allow one child window to be open and not allowing infinite
    number of child windows to be opened.
    
    Function Reader text box in win2 and giving the ability to automatically
    reading what function is being edited.
    
    Giving 'No Comment', and '>>' button functionality win2.
    
    Signed-off-by: SpencerGilliam <38114786+SpencerGilliam@users.noreply.github.com>

--- GUI.py
+++ GUI.py
@@ -1,13 +1,11 @@
-import tkinter
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, Tk, Toplevel
 import pprint  # added import
 import tkinter as tk
-from tkinter.filedialog import askopenfilename
 from tkinter.ttk import Combobox
 
 filelist = []  # added line
-root = tk.Tk()
+root: Tk = tk.Tk()
 root.geometry("500x500")
 
 mb = Menubutton(root, text="PyAutoDoc Menu")
@@ -20,56 +18,74 @@ class Language(tk.Frame):  # Allow you to choose your language
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
         choiceVar = tk.StringVar()
-        choices = ("Python", "C++", "C", "Ruby","C#","Go","Java","Javascript" #Maybe Set up dynamically later
-                   ,"PHP","Kotlin","Scala","Haskell","Lua","Rust","Perl") #Language Choices
+        choices = ("Python", "C++", "C", "Ruby", "C#", "Go", "Java", "Javascript"  # Maybe Set up dynamically later
+                   , "PHP", "Kotlin", "Scala", "Haskell", "Lua", "Rust", "Perl")  # Language Choices
         choiceVar.set(choices[0])
 
-        cb = Combobox(self, textvariable=choiceVar, values=choices, state="readonly") # Drop down menu
+        cb = Combobox(self, textvariable=choiceVar, values=choices, state="readonly")  # Drop down menu
 
         cb.pack()
+
 
 button = Button(text="Languages", width=30, command=lambda: Language)
 button.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 if __name__ == "__main__":
     Language(root).pack(fill="both", expand=True)
+
+
 ########################################################################################################################
-    def OpenFile(root, filelist): #Select Files function, produces a button, and allows you to choose multiple files
+    def OpenFile(root, filelist):  # Select Files function, produces a button, and allows you to choose multiple files
         filez = filedialog.askopenfilenames(parent=root, title='Select files')
-        filez = root.tk.splitlist(filez)
+        filez = root.tk.splitlist(filez) #splits file list
         filelist += filez  # added line
         Filebox(root, filelist)
-
 
 button = Button(text="Select Files", width=30, command=lambda: OpenFile(root, filelist))
 button.pack(padx=25, pady=20, side=tk.TOP)
 
-button2 = Button(text="Enter Commands", width=30, command=lambda: EnterComs(root))
+button2 = Button(text="Document", width=30, command=lambda: EnterComs(root))
 button2.pack(padx=25, pady=10, side=tk.TOP)
 
+
 #####################################################################################################
-def Filebox(root, filelist): #opens a child window that displays the current selected files
-	win1 = tk.Toplevel(bg = 'white')
-	win1.title('Selected files')
-	win1.geometry("600x600") #creates child window
-	textbox = Text(win1)
-	textbox.pack() #makes textbox
-	def redirector(inputStr):
-		textbox.insert(INSERT, inputStr) #function to redirect output
-	sys.stdout.write = redirector #set sysout to redirector
-	pp = pprint.PrettyPrinter(indent = 0)
-	pp.pprint(filelist) #print out list
-	textbox.config(state = DISABLED) #disable textbox so user cant mess with it
+def Filebox(root, filelist):  # opens a child window that displays the current selected files
+    win1 = tk.Toplevel(bg='white')
+    win1.title('Selected files')
+    win1.geometry("600x600")  # creates child window
+    textbox = Text(win1)
+    textbox.pack()  # makes textbox
+
+    def redirector(inputStr):
+        textbox.insert(INSERT, inputStr)  # function to redirect output
+
+    sys.stdout.write = redirector  # set sysout to redirector
+    pp = pprint.PrettyPrinter(indent=0)
+    pp.pprint(filelist)  # print out list
+    textbox.config(state=DISABLED)  # disable textbox so user cant mess with it
+
+
 ######################################################################################################
 
 ######################################################################################################
-def EnterComs(root): #opens a child window that allows user to type in (has no actual functionality yet)
-	win2 = tk.Toplevel(bg = 'white')
-	win2.title("Enter Commands")
-	win2.geometry("600x600") #creates child window
-	textbox = Text(win2)
-	textbox.pack()
-	textbox.insert(INSERT, "Enter your commands: ") #makes window textbox and adds text at beginning
+def EnterComs(root):  # opens a child window that allows user to type in
+    win2: Toplevel = tk.Toplevel(bg='white')
+    win2.title("Function Documentation")
+    win2.geometry("900x600")  # creates child window
+    textbox = Text(win2)
+    textbox.pack()
+    textbox.insert(INSERT, "Function name: \n"
+                           "Expected return type:\n"
+                           "Info on each of the parameters (Include how you have passed your ref/val):\n"
+                           "Possible errors this code may throw that can change possible variables:\n"
+                           "Description of Function: \n")  # makes window textbox and adds text at beginning
+
+    win1 = tk.Button(win2, text='No Comment')
+    win1.pack(padx=40, pady=30, side=tk.TOP)
+    win2 = tk.Button(win2, text='>>')
+    win2.pack(padx=25,pady=30,side=tk.TOP)
+
+
 #######################################################################################################
 
 
